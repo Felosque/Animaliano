b6fc352abec5094a2a5f623d08446d2dbfc49a50
Felipe Londono <felipe.londono@eccocar.com>
2021-11-15 22:01:59 -0500

    New pet structure

--- app/src/main/java/com/artificialbyte/animaliano/dto/pet/Pet.java
+++ app/src/main/java/com/artificialbyte/animaliano/dto/pet/Pet.java
@@ -1,7 +1,91 @@
 package com.artificialbyte.animaliano.dto.pet;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Pet {
 
+    private String uid;
+    private String uidFoundation;
+    private String description;
+    private String owner;
+    private ArrayList<String> photos;
+    private String birthDate;
+    private Map<String, String> vaccinationSchedule;
+    private ArrayList<PetRequest> petRequests;
 
+    public Pet(String uid, String uidFoundation, String description, String owner, ArrayList<String> photos, String birthDate, Map<String, String> vaccinationSchedule, ArrayList<PetRequest> petRequests) {
+        this.uid = uid;
+        this.uidFoundation = uidFoundation;
+        this.description = description;
+        this.owner = owner;
+        this.photos = photos;
+        this.birthDate = birthDate;
+        this.vaccinationSchedule = vaccinationSchedule;
+        this.petRequests = petRequests;
+    }
 
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUidFoundation() {
+        return uidFoundation;
+    }
+
+    public void setUidFoundation(String uidFoundation) {
+        this.uidFoundation = uidFoundation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public ArrayList<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(ArrayList<String> photos) {
+        this.photos = photos;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Map<String, String> getVaccinationSchedule() {
+        return vaccinationSchedule;
+    }
+
+    public void setVaccinationSchedule(Map<String, String> vaccinationSchedule) {
+        this.vaccinationSchedule = vaccinationSchedule;
+    }
+
+    public ArrayList<PetRequest> getPetRequests() {
+        return petRequests;
+    }
+
+    public void setPetRequests(ArrayList<PetRequest> petRequests) {
+        this.petRequests = petRequests;
+    }
 }
---
+++ app/src/main/java/com/artificialbyte/animaliano/dto/pet/PetRequest.java
@@ -0,0 +1,58 @@
+package com.artificialbyte.animaliano.dto.pet;
+
+public class PetRequest {
+
+    private String uid;
+    private String uidUser;
+    private String fullName;
+    private String contactPhone;
+    private String description;
+
+    public PetRequest(String uid, String uidUser, String fullName, String contactPhone, String description) {
+        this.uid = uid;
+        this.uidUser = uidUser;
+        this.fullName = fullName;
+        this.contactPhone = contactPhone;
+        this.description = description;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUidUser() {
+        return uidUser;
+    }
+
+    public void setUidUser(String uidUser) {
+        this.uidUser = uidUser;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
